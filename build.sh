ffe41fa560d06826414216068a342640a78e41fa
Abhishek <abhishek@pingsafe.com>
2023-05-25 11:28:36 +0530

    add: build script for all platforms

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# List of platforms and architectures to build for
+platforms=("linux" "darwin" "windows")
+architectures=("amd64" "386" "arm" "arm64")
+
+# Loop through each platform and architecture
+for platform in "${platforms[@]}"
+do
+    for architecture in "${architectures[@]}"
+    do
+        # Set the GOOS and GOARCH environment variables
+        export GOOS=$platform
+        export GOARCH=$architecture
+
+        # Generate the binary name based on platform and architecture
+        binary_name="builds/app_${platform}_${architecture}"
+
+        # Build the binary
+        go build -o $binary_name
+
+        # Print the build success message
+        echo "Built $binary_name"
+    done
+done
